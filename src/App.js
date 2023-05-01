54505871a142c5659e65ca17d4c275f63a1da14f
Shantanu Patel <shantu.patel@gmail.com>
2023-05-01 18:58:11 +0530

    addition of new project for introduction to CSS modules and styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import {useState} from "react";
 
+import './App.css';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
 
 const DUMMY_EXPENSES = [
   {
@@ -43,8 +47,55 @@ const App = () => {
     }
   };
 
+  // todo-app
+  const [courseGoals, setCourseGoals] = useState([
+    {text: 'Do all exercises!', id: 'g1'},
+    {text: 'Finish the course!', id: 'g2'}
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({text: enteredText, id: Math.random().toString()});
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{color: '#fff', textAlign: 'center'}}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
   return (
     <div>
+      <div>
+        <section id="goal-form">
+          <CourseInput onAddGoal={addGoalHandler} />
+        </section>
+        <section id="goals">
+          {content}
+          {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+        </section>
+      </div>
+
       {/* New Expense Form */}
       <NewExpense onAddExpense={addExpenseHandler} />
 
@@ -52,8 +103,6 @@ const App = () => {
       <Expenses expensesData={expenses} />
     </div>
   );
-
-
 }
 
 export default App;
